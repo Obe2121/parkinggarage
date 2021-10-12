bf6388a4e40a9ab462c506aa4975c896239e485d
Mike Oberdick <30874065+Obe2121@users.noreply.github.com>
2021-10-11 22:22:48 -0400

    update 2

--- garage.py
+++ garage.py
@@ -16,20 +16,49 @@ class Parking_Garage():
                 
             elif response.lower() == "leave":
                 self.leave_garage()
+
+            elif response.lower == "quit":
+                  break
         
 
     def take_ticket(self): #amount up or down by one
-        pass    
+            ticket_num = ticket
+            space_num = parking_space.pop(0)
+            current_tickets[ticket.pop(0)] = "unpaid"
+            print(ticket)
+            print(current_tickets)
 
+  
     def pay_for_parking(self): #how are you going to pay 
-        print("please pay")
+        while True:
+            response = input('Please input Ticket Number ')
+            if response == current_tickets[key]:
+                input("Press any key to pay")
+                current_ticket[ticket] = "paid"
+          
+            else:
+                print("Please input a valid Ticket Number")
+
     
     def leave_garage(self):#is your ticket paid or not
         while True:
-            response = input('please input ticket number ')
+            key = input('please input ticket number ')
+            if key == key in current_tickets and value == "paid":
+                current_ticket.pop(key)
+                ticket.append(key)
+                parking_space.append(key)
+                print("Have a good Day!")
 
+            else:
+                print("Please Pay your Ticket")
+            
+            
+              
 garage_program = Parking_Garage()
-ticket = (1,2,3,4,5,6,7,8,9,10)
-parking_space = (1,2,3,4,5,6,7,8,9,10)
-current_ticket = []
+current_tickets = {} 
+ticket = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+parking_space = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 garage_program.run_program()
+                 
+                  
+                  
